8106d808234aa105ea37168d3aedbd2588c45f69
auke-n <ozi@MacBook-Pro-Ozi.local>
2021-02-05 20:35:45 +0200

    BL-15 added ssl certificate to nginx

--- ec2_instances.tf
+++ ec2_instances.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web-server" {
-  ami                         = "ami-03c3a7e4263fd998c"
+  ami                         = "ami-0835479bdfce319a3"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public_subnet_web.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
--- security_groups.tf
+++ security_groups.tf
@@ -14,6 +14,12 @@ resource "aws_security_group" "web_sg" {
     to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 443
+    protocol    = "tcp"
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     protocol    = "-1"
