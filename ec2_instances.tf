8cc7550c4abfe65bf2234977cb245869992d12f7
auke-n <brs.morozov@gmail.com>
2021-02-12 14:33:32 +0200

    bl-17 modified security group to allow traffic between instances

--- ec2_instances.tf
+++ ec2_instances.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "test" {
   ami                         = "ami-0a6dc7529cd559185"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.test-subnet.id
-  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  vpc_security_group_ids      = [aws_security_group.test_sg.id]
   key_name                    = "Frankfurt"
   associate_public_ip_address = true
 
